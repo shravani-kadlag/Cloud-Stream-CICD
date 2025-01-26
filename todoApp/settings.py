3dc04e1ca826fba3659865be983b85ccdda61c1b
shravani-kadlag <162776797+shravani-kadlag@users.noreply.github.com>
2025-01-26 23:59:43 +0530

    Update settings.py

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'todos.apps.TodosConfig',
+    'todos.app.TodosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
